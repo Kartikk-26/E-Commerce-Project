7d2dc6ab3d3257f48bfc3fc0a5a00aa055b661ec
Kartikjain2626 <kartikjain2626@gmail.com>
2024-09-19 13:53:01 +0530

    Frontend Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import Signup from './pages/Signup'
+
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path = '/register' element = {<Signup/>}/>
+      </Routes>
+    </div>
+  )
+}
